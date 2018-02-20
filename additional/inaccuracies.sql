01359bb35b7b859d507e317ba39ce6e71891509a
Fabio Elia Locatelli <fabioelialocatelli@yandex.com>
2018-02-20 13:59:26 +1300

    Inaccuracies Revised
    The script has been revised and minor formatting changes have been applied. Logic remained untouched.

--- additional/inaccuracies.sql
+++ additional/inaccuracies.sql
@@ -1,19 +1,75 @@
-USE eridanus;
-DROP VIEW IF EXISTS riverInaccuracies;
-CREATE VIEW riverInaccuracies AS
+/**
+* @author FABIO ELIA LOCATELLI
+* @creationDate 08/12/2016
+* @revisionDate 20/02/2018
+* @purpose EXPOSING INACCURACIES
+*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+USE bayer;
+
+DROP VIEW IF EXISTS VIEW_stellarInaccuracies;
+CREATE VIEW VIEW_stellarInaccuracies AS
     SELECT 
-        river.localName, river.sourceLocation, river.mouthLocation
+        parameterAlias.designation AS bayerDesignation,
+        nameAlias.denomination AS properName,
+        parameterAlias.parallax,
+        parameterAlias.parsecs,
+        parameterAlias.lightYears,
+        parameterAlias.apparentMagnitude,
+        parameterAlias.absoluteMagnitude,
+        parameterAlias.bolometricMagnitude,
+        parameterAlias.solarDiameter,
+        parameterAlias.absoluteLuminosity,
+        parameterAlias.bolometricLuminosity,
+        parameterAlias.solarMass,
+        parameterAlias.innerBoundary,
+        parameterAlias.outerBoundary,
+        parameterAlias.gregorianYear,
+        parameterAlias.spectralClass,
+        parameterAlias.stellarCategory
     FROM
-        river
+        bayer.TABLE_stellarName nameAlias
+            RIGHT JOIN
+        bayer.TABLE_stellarParameters parameterAlias ON nameAlias.designation = parameterAlias.designation
     WHERE
-        localName IN ('Ganges' , 'Léna',
-            'Mekong',
-            'Niger',
-            'Nile',
-            'Ob',
-            'Orinoco',
-            'Volga',
-            'Yangtze',
-            'Yukon',
-            'Zambezi')
-    ORDER BY localName;
+        parameterAlias.designation IN ('α Andromedae' , 'α Cassiopeiae',
+            'α Columbae',
+            'α Fornacis',
+            'α Gruis',
+            'α Persei',
+            'α Pyxidis',
+            'β Gruis',
+            'β Phoenicis',
+            'γ Cassiopeiae',
+            'γ Cephei',
+            'γ Draconis',
+            'γ Lyrae',
+            'γ Persei',
+            'γ Piscium',
+            'δ Cygni',
+            'δ Pegasi',
+            'δ Persei',
+            'δ Virginis',
+            'ε Aquilae',
+            'ε Persei',
+            'ζ Aquilae',
+            'ζ Centauri',
+            'η Carinae',
+            'η Ceti',
+            'η Geminorum',
+            'η Persei',
+            'θ Cassiopeiae',
+            'ι Draconis',
+            'ι Geminorum',
+            'κ Crucis',
+            'κ Geminorum',
+            'κ Persei',
+            'λ Geminorum',
+            'λ Herculis',
+            'μ Cassiopeiae',
+            'μ Geminorum',
+            'μ Velorum',
+            'π Puppis',
+            'σ Scorpii',
+            'τ Scorpii')
+    ORDER BY bayerDesignation;
